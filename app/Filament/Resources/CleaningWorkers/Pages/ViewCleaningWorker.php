35a5c5549ee8f263ac5dd15219d9b5eedc4ee782
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-18 19:03:21 +0300

    Keep cleaning worker view inside cleaning resource

--- app/Filament/Resources/CleaningWorkers/Pages/ViewCleaningWorker.php
+++ app/Filament/Resources/CleaningWorkers/Pages/ViewCleaningWorker.php
@@ -5,22 +5,12 @@ declare(strict_types=1);
 namespace App\Filament\Resources\CleaningWorkers\Pages;
 
 use App\Filament\Resources\CleaningWorkers\CleaningWorkerResource;
-use App\Filament\Resources\Workers\WorkerResource;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
 final class ViewCleaningWorker extends ViewRecord
 {
     protected static string $resource = CleaningWorkerResource::class;
-
-    public function mount(int|string $record): void
-    {
-        parent::mount($record);
-
-        if ($this->record->worker) {
-            $this->redirect(WorkerResource::getUrl('view', ['record' => $this->record->worker]));
-        }
-    }
 
     protected function getHeaderActions(): array
     {
