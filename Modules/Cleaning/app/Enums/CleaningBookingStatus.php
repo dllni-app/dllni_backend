335ade765b22bcaad747c756683524f0696b5057
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 02:30:08 +0300

    Add under dispute cleaning booking status

--- Modules/Cleaning/app/Enums/CleaningBookingStatus.php
+++ Modules/Cleaning/app/Enums/CleaningBookingStatus.php
@@ -13,6 +13,7 @@ enum CleaningBookingStatus: string
     case InProgress = 'in_progress';
     case AwaitingCustomerCompletion = 'awaiting_customer_completion';
     case TimeExtensionRequested = 'time_extension_requested';
+    case UnderDispute = 'under_dispute';
     case Completed = 'completed';
     case Cancelled = 'cancelled';
 
