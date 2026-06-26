fcb8d72d6350ee5da2d6298ae5fe7a6813dbc1f6
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-26 05:05:37 +0300

    Add restaurant owner order status request

---
+++ Modules/Resturants/app/Http/Requests/RestaurantOwner/OwnerOrderStatusRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Resturants\Http\Requests\RestaurantOwner;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class OwnerOrderStatusRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'status' => 'required|string|in:accepted,preparing,ready_for_pickup,picked_up,completed,cancelled',
+            'reason' => 'nullable|string|max:255',
+            'customerMessage' => 'nullable|string|max:500',
+        ];
+    }
+}
