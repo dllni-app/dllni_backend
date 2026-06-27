8cc926c18f99a5425dbaa5bf4f9add5b3456feee
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-27 04:13:57 +0300

    Revert loyalty automation rules page copy

--- app/Filament/Resources/CleaningAutomationRules/Pages/ListCleaningAutomationRules.php
+++ app/Filament/Resources/CleaningAutomationRules/Pages/ListCleaningAutomationRules.php
@@ -14,7 +14,7 @@ final class ListCleaningAutomationRules extends ListRecords
 
     public function getSubheading(): ?string
     {
-        return 'Create rules such as 100 completed hours within 2 months. The system only creates a pending member bonus; admin activation is required.';
+        return __('cleaning_admin.pages.automation_rules.list');
     }
 
     protected function getHeaderActions(): array
