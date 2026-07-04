2e59c10b0c64fc1ebbf95bea2af78ec72474e6ee
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-04 15:06:30 +0300

    Add database imports to price normalizer

--- database/seeders/SyrianPoundSeedPriceNormalizer.php
+++ database/seeders/SyrianPoundSeedPriceNormalizer.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 final class SyrianPoundSeedPriceNormalizer extends Seeder
 {
