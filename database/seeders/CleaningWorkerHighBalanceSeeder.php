7df154e33d96f3042801fdedd41c8d14b46ddd5a
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-04 14:41:50 +0300

    Add high balance seeder for cleaning workers

---
+++ database/seeders/CleaningWorkerHighBalanceSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+final class CleaningWorkerHighBalanceSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Placeholder seeder.
+    }
+}
