5361ab09942a282a4191a08c93c85b1fbc296fe6
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-09 17:39:00 +0300

    Confirm cleaning start codes per worker

--- Modules/User/app/Http/Controllers/API/UserCleaningOrderStartVerificationConfirmController.php
+++ Modules/User/app/Http/Controllers/API/UserCleaningOrderStartVerificationConfirmController.php
@@ -7,18 +7,18 @@ namespace Modules\User\Http\Controllers\API;
 use Illuminate\Http\JsonResponse;
 use Modules\Cleaning\Http\Resources\CleaningBookingResource;
 use Modules\Cleaning\Models\CleaningBooking;
+use Modules\Cleaning\Services\CleaningBookingWorkerSecurityCodeService;
 use Modules\User\Http\Requests\UserCleaningOrderStartVerificationConfirmRequest;
-use Modules\User\Services\UserCleaningOrderService;
 
 final class UserCleaningOrderStartVerificationConfirmController
 {
-    public function __invoke(UserCleaningOrderStartVerificationConfirmRequest $request, int $order, UserCleaningOrderService $service): JsonResponse
+    public function __invoke(UserCleaningOrderStartVerificationConfirmRequest $request, int $order, CleaningBookingWorkerSecurityCodeService $service): JsonResponse
     {
         $model = CleaningBooking::query()
             ->where('customer_id', $request->user()->id)
             ->findOrFail($order);
 
-        $updated = $service->confirmStartVerification($model, $request->validated('code'));
+        $updated = $service->confirmForCustomer($model, $request->validated('code'));
         $updated->load(['worker.user', 'workerAssignments.worker.user', 'rooms.assignedWorker.user', 'timeWarnings', 'disputes', 'addons', 'billingPolicy']);
 
         return CleaningBookingResource::make($updated)->additional([
