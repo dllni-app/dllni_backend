3e3f5a1eb2c1eb441a90f5bcc928019f32d86790
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-13 07:16:02 +0300

    Translate edit system manager page

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -14,6 +14,11 @@ final class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
+    public function getTitle(): string
+    {
+        return 'تعديل مدير النظام';
+    }
+
     public function mutateFormDataBeforeFill(array $data): array
     {
         $data['role_id'] = $this->record->roles()->first()?->id;
@@ -25,14 +30,15 @@ final class EditUser extends EditRecord
     {
         $roleId = $this->form->getState()['role_id'] ?? null;
         $role = $roleId ? Role::find($roleId) : null;
+
         $this->record->syncRoles($role ? [$role->name] : []);
     }
 
     protected function getHeaderActions(): array
     {
         return [
-            ViewAction::make(),
-            DeleteAction::make(),
+            ViewAction::make()->label('عرض'),
+            DeleteAction::make()->label('حذف'),
         ];
     }
 }
