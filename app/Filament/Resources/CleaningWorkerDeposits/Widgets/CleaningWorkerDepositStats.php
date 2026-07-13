ede576da1c99e90496932dbaea1b1a876da69d8a
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-13 05:03:21 +0300

    Align statistic cards with the four transaction types

--- app/Filament/Resources/CleaningWorkerDeposits/Widgets/CleaningWorkerDepositStats.php
+++ app/Filament/Resources/CleaningWorkerDeposits/Widgets/CleaningWorkerDepositStats.php
@@ -24,21 +24,21 @@ final class CleaningWorkerDepositStats extends StatsOverviewWidget
             ->value('total');
         $totalDeposits = $this->sumByType('deposit');
         $totalSettlements = $this->sumByType('settlement');
-        $totalRefunds = $this->sumByType('refund') + $this->sumByType('withdrawal');
+        $totalRefunds = $this->sumByType('refund');
 
         return [
             Stat::make(__('Total current debt'), self::money($currentDebt))
                 ->icon('heroicon-o-exclamation-triangle')
                 ->color($currentDebt > 0 ? 'danger' : 'success'),
             Stat::make(__('Total deposits'), self::money($totalDeposits))
-                ->description(__('Manual deposit transactions'))
+                ->description(__('Deposit transactions'))
                 ->icon('heroicon-o-arrow-down-tray')
                 ->color('success'),
             Stat::make(__('Total settlements'), self::money($totalSettlements))
                 ->description(__('Debt settlement transactions'))
                 ->icon('heroicon-o-check-circle')
                 ->color('primary'),
-            Stat::make(__('Refunds and withdrawals'), self::money($totalRefunds))
+            Stat::make(__('Total refunds'), self::money($totalRefunds))
                 ->icon('heroicon-o-arrow-uturn-left')
                 ->color('warning'),
         ];
