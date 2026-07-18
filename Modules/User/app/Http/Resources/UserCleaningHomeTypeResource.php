c996bd5287fe67318f04f6a56dced11a03861633
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-18 22:37:59 +0300

    Add cleaning home type API resource

---
+++ Modules/User/app/Http/Resources/UserCleaningHomeTypeResource.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Cleaning\Models\CleaningHomeType;
+
+/**
+ * @mixin CleaningHomeType
+ */
+final class UserCleaningHomeTypeResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        /** @var CleaningHomeType $type */
+        $type = $this->resource;
+
+        return [
+            'id' => $type->id,
+            'section' => $type->section,
+            'code' => $type->code,
+            'title' => $type->title,
+            'imageUrl' => $type->imageUrl(),
+            'sortOrder' => $type->sort_order,
+        ];
+    }
+}
