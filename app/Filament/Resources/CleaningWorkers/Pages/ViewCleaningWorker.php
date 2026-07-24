9eeaefc1b9c704ed043d05435ec4b1fbd10d5119
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-24 10:52:44 +0300

    show worker financial penalty statistics

--- app/Filament/Resources/CleaningWorkers/Pages/ViewCleaningWorker.php
+++ app/Filament/Resources/CleaningWorkers/Pages/ViewCleaningWorker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\Resources\CleaningWorkers\Pages;
 
 use App\Filament\Resources\CleaningWorkers\CleaningWorkerResource;
+use App\Filament\Resources\CleaningWorkers\Widgets\CleaningWorkerPenaltyStats;
 use App\Filament\Resources\Workers\Support\WorkerSuspensionActions;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
@@ -12,6 +13,13 @@ use Filament\Resources\Pages\ViewRecord;
 final class ViewCleaningWorker extends ViewRecord
 {
     protected static string $resource = CleaningWorkerResource::class;
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            CleaningWorkerPenaltyStats::class,
+        ];
+    }
 
     protected function getHeaderActions(): array
     {
