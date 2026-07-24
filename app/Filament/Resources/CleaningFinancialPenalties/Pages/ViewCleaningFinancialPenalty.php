c81aebbb93ea4fcae26dd744b6a0e06feae1ca34
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-24 10:51:07 +0300

    add cleaning penalty view page

---
+++ app/Filament/Resources/CleaningFinancialPenalties/Pages/ViewCleaningFinancialPenalty.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\CleaningFinancialPenalties\Pages;
+
+use App\Filament\Resources\CleaningFinancialPenalties\CleaningFinancialPenaltyResource;
+use Filament\Resources\Pages\ViewRecord;
+
+final class ViewCleaningFinancialPenalty extends ViewRecord
+{
+    protected static string $resource = CleaningFinancialPenaltyResource::class;
+}
