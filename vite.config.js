7724bddd61b3f83ece8e7c5774be976fc73c1bfe
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-03 16:17:33 +0300

    build: compile shared Alnadha Filament theme

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/filament/cleaning-admin/theme.css',
+                'resources/css/filament/alnadha/theme.css',
                 'resources/js/app.js',
             ],
             refresh: true,
