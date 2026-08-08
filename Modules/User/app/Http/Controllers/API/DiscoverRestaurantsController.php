fad6414dd8c89a023bae73069bdb7dba24bd675d
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-08 14:39:17 +0300

    Track restaurant merchant searches separately

--- Modules/User/app/Http/Controllers/API/DiscoverRestaurantsController.php
+++ Modules/User/app/Http/Controllers/API/DiscoverRestaurantsController.php
@@ -31,6 +31,7 @@ final class DiscoverRestaurantsController
                 $this->popularSearches->record(
                     UserPopularSearchService::RESTAURANT,
                     $search,
+                    UserPopularSearchService::MERCHANTS,
                 );
             }
 
