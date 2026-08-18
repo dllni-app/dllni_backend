0a4394bbf7f118f5d41141c311b8e50d0bb8860e
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-18 13:20:18 +0300

    Use customer cleaning price resource for extension responses

--- Modules/User/app/Http/Controllers/API/UserCleaningOrderCompletionExtendTimeController.php
+++ Modules/User/app/Http/Controllers/API/UserCleaningOrderCompletionExtendTimeController.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Modules\User\Http\Controllers\API;
 
 use Illuminate\Http\JsonResponse;
-use Modules\Cleaning\Http\Resources\CleaningBookingResource;
 use Modules\Cleaning\Models\CleaningBooking;
 use Modules\Cleaning\Services\CleaningBookingWorkerCompletionService;
 use Modules\User\Http\Requests\UserCleaningOrderCompletionExtendTimeRequest;
+use Modules\User\Http\Resources\UserCleaningBookingResource;
 
 final class UserCleaningOrderCompletionExtendTimeController
 {
@@ -28,7 +28,7 @@ final class UserCleaningOrderCompletionExtendTimeController
         $updated = $result['booking'];
         $updated->load(['worker.user', 'workerAssignments.worker.user', 'rooms.assignedWorker.user', 'timeWarnings', 'disputes', 'addons', 'billingPolicy']);
 
-        return CleaningBookingResource::make($updated)->additional([
+        return UserCleaningBookingResource::make($updated)->additional([
             'message' => __('Extension request sent successfully.'),
             'extensionPricing' => $result['extensionPricing'],
         ])->response();
